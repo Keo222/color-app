8e9a5bacea93ca4e8d7aa72d6d50d44029a189dd
Keo222 <kyote222@gmail.com>
2021-08-10 14:24:54 -0700

    created app, added palette and starter colors

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Palette extends Component {
+    render() {
+        return (
+            <div className="palette">
+                {/* navbar */}
+                <div className="Palette-colors">
+                    {/* color boxes */}
+                </div>
+                {/* footer */}
+            </div>
+        )
+    }
+}
