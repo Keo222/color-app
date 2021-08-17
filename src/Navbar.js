b484a6f20b26ea47daf8363587b38ebcdcb4f209
Keo222 <kyote222@gmail.com>
2021-08-17 11:50:13 -0700

    can now change color format

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,31 +1,57 @@
 import React, { Component } from 'react';
 
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
 import './Navbar.css';
 
 export default class Navbar extends Component {
-    render() {
-        const {level, changeLevel} = this.props;
-        return (
-            <header className="Navbar">
-                <div className="logo">
-                    <a href="/">reactcolorpicker</a>
-                </div>
-                <div className="slider-container">
-                    <span>Level: {level}</span>
-                    <div className="slider">
-                        <Slider 
-                            defaultValue={level}
-                            min={100}
-                            max={900}
-                            step={100}
-                            onAfterChange={changeLevel}    
-                        />
-                    </div>
-                </div>
-            </header>
-        )
+  constructor(props){
+    super(props);
+      this.state = {format: "hex"};
+      this.handleChange = this.handleChange.bind(this);
     }
+    handleChange(e){
+      this.setState({format: e.target.value});
+      this.props.changeFormat(e.target.value);
+    }
+  render() {
+    const {level, changeLevel} = this.props;
+    const {format} = this.state;
+    return (
+      <header className="Navbar">
+        <div className="logo">
+          <a href="/">reactcolorpicker</a>
+        </div>
+        <div className="slider-container">
+          <span>Level: {level}</span>
+          <div className="slider">
+            <Slider 
+              defaultValue={level}
+              min={100}
+              max={900}
+              step={100}
+              onAfterChange={changeLevel}    
+            />
+          </div>
+        </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">
+              HEX - #ffffff
+            </MenuItem>
+            <MenuItem value="rgb">
+              RGB - (255,255,255)
+            </MenuItem>
+            <MenuItem value="rgba">
+              RGBA - (255,255,255, 1.0)
+            </MenuItem>
+          </Select>
+        </div>
+      </header>
+    )
+  }
 }
