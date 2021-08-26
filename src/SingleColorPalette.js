b912c6c3bc9df23c2f939b41ed3deea3cbd1318f
Keo222 <kyote222@gmail.com>
2021-08-26 12:02:52 -0700

    added go back box and changed single palette boxes style

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -3,6 +3,8 @@ import ColorBox from './ColorBox';
 import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
 
+import { Link } from 'react-router-dom';
+
 export default class SingleColorPalette extends Component {
     constructor(props){
         super(props);
@@ -27,17 +29,19 @@ export default class SingleColorPalette extends Component {
         this.setState({format: val})
     }
     render() {
-        const { paletteName, emoji } = this.props.palette;
+        const { paletteName, emoji, id } = this.props.palette;
         const { format } = this.state;
         const colorBoxes = this._shades.map(c => (
             <ColorBox name={c.name} key={c.name} background={c[format]} showLink={false} />
         ));
         return (
-            <div className="Palette">
+            <div className="SingleColorPalette Palette">
             <Navbar changeFormat={this.changeFormat} showSlider={false}/>
-                <h1>Single Color Palette!!</h1>
                 <div className="Palette-colors">
                     {colorBoxes}
+                    <div className="go-back ColorBox">
+                        <Link to={`/palette/${id}`} className="back-button">GO BACK</Link>
+                    </div>
                 </div>
                 <PaletteFooter paletteName={paletteName} emoji={emoji} />
             </div>
